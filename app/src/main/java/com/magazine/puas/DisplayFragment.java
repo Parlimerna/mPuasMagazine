322df4e518b29b06a400698c2f0edc1edfaf92c4
Wahyu Adhi Ozcan <wahyuad10541@gmail.com>
2016-04-14 19:48:25 +0700

    Setup New Project
    Create the project for the first time

---
+++ app/src/main/java/com/magazine/puas/DisplayFragment.java
@@ -0,0 +1,35 @@
+package com.magazine.puas;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.joanzapata.pdfview.PDFView;
+
+public class DisplayFragment extends Fragment {
+    private static DisplayFragment fragment;
+
+    public static DisplayFragment newInstance() {
+        //if (null == fragment) fragment = new DisplayFragment();
+        //return fragment;
+        return new DisplayFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.content_page, container, false);
+
+        String  pdfName = "dummy.pdf";
+        PDFView pdfView = (PDFView) view.findViewById(R.id.pdfview);
+        pdfView.fromAsset(pdfName)
+                .defaultPage(0)
+                .swipeVertical(true)
+                .showMinimap(true)
+                .enableSwipe(true)
+                .load();
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/magazine/puas/MainActivity.java
@@ -0,0 +1,67 @@
+package com.magazine.puas;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.joanzapata.pdfview.PDFView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        ViewPager               mPager        = (ViewPager) findViewById(R.id.pager);
+        ScreenSlidePagerAdapter mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return DisplayFragment.newInstance();
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+}
